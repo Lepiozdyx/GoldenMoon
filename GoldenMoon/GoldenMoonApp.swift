2da1e843b8ddaa6bebd866fdbf56724187dca524
Developer <Lepiozdyx@gmail.com>
2025-05-05 15:56:20 -0300

    Initial Commit

---
+++ GoldenMoon/GoldenMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldenMoonApp.swift
+//  GoldenMoon
+//
+//  Created by Alex on 05.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct GoldenMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
